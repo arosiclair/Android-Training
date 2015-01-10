692f437ece5788926bd32791bbbec4d26a728a6a
arosiclair <arosiclair@gmail.com>
2015-01-09 20:45:49 -0500

    Initial commit. On "Styling the Action Bar".

---
+++ app/src/main/java/rosiclair/andrew/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,29 @@
+package rosiclair.andrew.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Get extra message
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Display message as a text view
+        TextView text = new TextView(this);
+        text.setText(message);
+        text.setTextSize(40);
+        //Set the new text view as the activity's root view.
+        setContentView(text);
+
+        //Set the up button icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
